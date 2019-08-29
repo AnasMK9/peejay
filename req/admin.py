ea1431505b969f9073512b8ded598b9c85f5589b
Anas <a.khudeirat@hotmail.com>
2019-08-29 16:23:12 +0300

    asd

--- req/admin.py
+++ req/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import reqT
+from . import models
 # Register your models here.
-admin.site.register(reqT)
+admin.site.register(models.req1)
+admin.site.register(models.req2)
+admin.site.register(models.req3)
+admin.site.register(models.req4)
+admin.site.register(models.req5)
+admin.site.register(models.req6)
+admin.site.register(models.req7)
+admin.site.register(models.req11)
+admin.site.register(models.reqT)
